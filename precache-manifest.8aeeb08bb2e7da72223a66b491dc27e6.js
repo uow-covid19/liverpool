df69422132b7e2b9331be790e8a04b4736713db7
uow-covid19 <buildbot@example.com>
2020-09-28 13:32:18 +0000

    Update from https://github.com/uow-covid19/uow-covid19.github.io.dev/commit/07c33e2660acbabea3daf22129ee0af779b3e6c3)

---
+++ precache-manifest.8aeeb08bb2e7da72223a66b491dc27e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d428591441174cc64abe7740e832b0dc",
+    "url": "/liverpool/index.html"
+  },
+  {
+    "revision": "be7c303be6b5ec1530f0",
+    "url": "/liverpool/static/css/main.cbe442cf.chunk.css"
+  },
+  {
+    "revision": "9bfec86421d95dc3c756",
+    "url": "/liverpool/static/js/2.6892d898.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/liverpool/static/js/2.6892d898.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be7c303be6b5ec1530f0",
+    "url": "/liverpool/static/js/main.613a6eb8.chunk.js"
+  },
+  {
+    "revision": "665b80ef4a133f730551",
+    "url": "/liverpool/static/js/runtime-main.fc8e2fe1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.14a80cf546bbda08ea7d992909bae479.js"
+  "/liverpool/precache-manifest.8aeeb08bb2e7da72223a66b491dc27e6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/liverpool/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
